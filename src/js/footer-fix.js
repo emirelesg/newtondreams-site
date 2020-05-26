ddb9ee458f68c08854612ed6f6ff3f4ac9752ebc
Enrique Mireles <enrique.mireles@udem.edu>
2020-05-26 17:04:48 -0500

    removed weird chars

--- src/js/footer-fix.js
+++ src/js/footer-fix.js
@@ -2,12 +2,12 @@
  * This function makes sure that the footer is sticky or fixed at the bottom of the page.
  * It adds a padding to the body element to achieve this.
  */
+function fixFooter() {
+  var height = $('.footer').outerHeight();
+  $('body').css('paddingBottom', height + 'px');
+}
 
- $(function(){
-    function fixFooter() {
-        var height = $('.footer').outerHeight();
-        $('body').css('paddingBottom', height+'px');
-    }
-    window.addEventListener("resize", fixFooter, false);
-    fixFooter();
+$(function () {
+  window.addEventListener('resize', fixFooter, false);
+  fixFooter();
 });
