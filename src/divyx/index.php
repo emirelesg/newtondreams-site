63c8e4954a16f244f349dc5eb9021cad6bd39c37
Enrique Mireles <enrique.mireles@udem.edu>
2020-05-26 17:06:32 -0500

    removed popper

--- src/divyx/index.php
+++ src/divyx/index.php
@@ -309,7 +309,6 @@
     </footer>
 
     <script src="https://code.jquery.com/jquery-3.3.1.min.js" integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8=" crossorigin="anonymous"></script>
-    <script src="../js/popper.min.js"></script>
     <script src="../js/feather.min.js"></script>
     <script src="../js/bootstrap.bundle.min.js"></script>
     <script src="../js/footer-fix.js"></script>
